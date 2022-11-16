ca22ccbdb27173459e5271637d1a929f9b179674
Jomi Benitez <josebenitez549@gmail.com>
2022-11-16 19:49:42 -0300

    clase 22 react

---
+++ clasereact22/src/App.js
@@ -0,0 +1,13 @@
+import Headers from "./componentes/layout/Headers";
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Headers></Headers>
+      <Headers/>
+    </div>
+  );
+}
+
+export default App;
---
+++ clasereact22/src/componentes/layout/Headers.js
@@ -0,0 +1,10 @@
+const Headers = (props) =>{
+    return(
+        <div>
+        <p>hola soy el Header</p>
+        <p>lorem ipsum dolor sit amet, consectetur adip ajsdjfkasdfkasdflm jasdjvkasdf</p>
+        </div>
+    )
+}
+
+export default Headers;
